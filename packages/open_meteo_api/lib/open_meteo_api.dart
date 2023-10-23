74c16f562b0ae2cead4bb58530dc0ed2a8e639da
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-10-23 10:25:33 +0300

    implementing and adding some new files to the open meteo package I created

--- packages/open_meteo_api/lib/open_meteo_api.dart
+++ packages/open_meteo_api/lib/open_meteo_api.dart
@@ -1,7 +1,3 @@
 library open_meteo_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/models.dart';
---
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -0,0 +1,13 @@
+class Location {
+  const Location({
+    required this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final int id;
+  final String name;
+  final double latitude;
+  final double longitude;
+}

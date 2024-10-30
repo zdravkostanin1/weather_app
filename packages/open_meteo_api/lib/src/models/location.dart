14ac4f3712590b208946da84458cc5afd90c128b
Zdravko Stanin <zdravko.stanin@gmail.com>
2024-10-30 11:55:08 +0200

    initial commit

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   const Location({
     required this.id,
@@ -5,6 +10,8 @@ class Location {
     required this.latitude,
     required this.longitude,
   });
+
+  factory Location.fromJson(Map<String, dynamic> json) => _$LocationFromJson(json);
 
   final int id;
   final String name;
--- packages/open_meteo_api/test/open_meteo_api_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -1,12 +1,146 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 import 'package:open_meteo_api/open_meteo_api.dart';
 
+class MockHttpClient extends Mock implements http.Client {
+  @override
+  void close() {
+    // TODO: implement close
+  }
+
+  @override
+  Future<http.Response> delete(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> get(Uri url, {Map<String, String>? headers}) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> head(Uri url, {Map<String, String>? headers}) {
+    // TODO: implement head
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> patch(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    // TODO: implement patch
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> post(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> put(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    // TODO: implement put
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> read(Uri url, {Map<String, String>? headers}) {
+    // TODO: implement read
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
+    // TODO: implement readBytes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    // TODO: implement send
+    throw UnimplementedError();
+  }
+}
+
+class MockResponse extends Mock implements http.Response {
+  @override
+  // TODO: implement body
+  String get body => throw UnimplementedError();
+
+  @override
+  // TODO: implement bodyBytes
+  Uint8List get bodyBytes => throw UnimplementedError();
+
+  @override
+  // TODO: implement contentLength
+  int? get contentLength => throw UnimplementedError();
+
+  @override
+  // TODO: implement headers
+  Map<String, String> get headers => throw UnimplementedError();
+
+  @override
+  // TODO: implement isRedirect
+  bool get isRedirect => throw UnimplementedError();
+
+  @override
+  // TODO: implement persistentConnection
+  bool get persistentConnection => throw UnimplementedError();
+
+  @override
+  // TODO: implement reasonPhrase
+  String? get reasonPhrase => throw UnimplementedError();
+
+  @override
+  // TODO: implement request
+  http.BaseRequest? get request => throw UnimplementedError();
+
+  @override
+  // TODO: implement statusCode
+  int get statusCode => throw UnimplementedError();
+}
+
+class FakeUri extends Fake implements Uri {}
+
 void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  // });
+  group('Location', () {
+    group('fromJson', () {
+      test('returns correct Location object', () {
+        expect(
+          Location.fromJson(
+            <String, dynamic>{
+              'id': 4887398,
+              'name': 'Chicago',
+              'latitude': 41.85003,
+              'longitude': -87.65005,
+            },
+          ),
+          isA<Location>()
+              .having((w) => w.id, 'id', 4887398)
+              .having((w) => w.name, 'name', 'Chicago')
+              .having((w) => w.latitude, 'latitude', 41.85003)
+              .having((w) => w.longitude, 'longitude', -87.65005),
+        );
+      });
+    });
+  });
+  group('Weather', () {
+    group('fromJson', () {
+      test('returns correct Weather object', () {
+        expect(
+          Weather.fromJson(
+            <String, dynamic>{'temperature': 15.3, 'weathercode': 63},
+          ),
+          isA<Weather>()
+              .having((w) => w.temperature, 'temperature', 15.3)
+              .having((w) => w.weatherCode, 'weatherCode', 63),
+        );
+      });
+    });
+  });
 }
